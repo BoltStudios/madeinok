ae2bdd0d58133d46ac893a12666908261dcde12c
tforbus <tristinforbus@gmail.com>
2013-07-22 22:04:02 -0500

    sample error message on invalid input

--- public/javascripts/controllers/listings/CreateCtrl.js
+++ public/javascripts/controllers/listings/CreateCtrl.js
@@ -2,6 +2,17 @@ function CreateCtrl($scope, $location, $http, $routeParams) {
 	// Don't create the form data here. Do it on the page and save yourself troubles.
 	$scope.formData = {}
 
+	$scope.companyTypes = [
+    	{type: 'Technology'}
+      , {type: 'Agency'}
+      , {type: 'Dev Shop'}
+      , {type: 'Service Firm'}
+      , {type: 'Incubator/Accelerator'}
+      , {type: 'VC/Angel Fund'}
+      , {type: 'Other'}
+  	]
+	$scope.companyType = $scope.companyTypes[0]
+
 	$scope.submit = function() {
 		console.log('form data is ' + JSON.stringify($scope.formData))
 		$http.post('/api/listings/create', $scope.formData).success(function(response) {
