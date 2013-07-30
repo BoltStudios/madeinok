bf18855cad66d27986f0894b162fa6ac74d3e25f
tforbus <tristinforbus@gmail.com>
2013-07-30 15:38:20 -0500

    bcrypt

---
+++ models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+  , bcrypt = require('bcrypt')
+  , ObjectId = mongoose.Schema.ObjectId
+
+var UserSchema = new mongoose.Schema({
+	username: {type: String, required: true}
+  , email: {type: String, required: true, lowercase: true}
+  , password: {type: String, required: true}
+})
+
+UserSchema.methods.setPassword = function(passwordString) {
+	this.password = bcrypt.hashSync(passwordString, 10)
+}
+
+UserSchema.methods.isCorrectPassword = function(passwordString) {
+	return bcrypt.compareSync(passwordSring, this.password)
+}
+
+module.exports = mongoose.model('Users', UserSchema)
