a70ed1ff3411ec41a7da615838f545d3eb2fed28
tforbus <tristinforbus@gmail.com>
2013-08-21 15:48:41 -0500

    added keys again

---
+++ keys.js
@@ -0,0 +1,21 @@
+GLOBAL.cloudinaryConfig = {
+	cloud_name: 'hfjjoialf',
+	api_key: '464752786413982', 
+  	api_secret: '55mwg-8lfpQQZqx9q6zxj66umDY'
+}
+
+GLOBAL.twitter = {
+	consumerKey: 'rcFo0Fee6JiukSK3vfGZlA',
+	consumerSecret: 'AK3RBfUzKizK7bZDyJ1u6PsKvKXUL0bNjvE0pKlfis',
+	callbackURL: '/auth/twitter/callback'
+}
+
+GLOBAL.facebook = {
+	clientID: '411315155655493',
+	clientSecret: '8f0fc227c392111c81da2eb68f5a9f8e',
+	callbackURL: '/auth/facebook/callback'
+}
+
+GLOBAL.mongolab = {
+	URI: 'mongodb://heroku_app17523224:8jb1ejmd4rgnljv0if716mqpla@ds041208.mongolab.com:41208/heroku_app17523224'
+}
