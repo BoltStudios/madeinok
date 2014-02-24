dd5f4291a8f5aaab2dff84454f541e785bc6dbd0
calekennedy <ckennedy@mtmrecognition.com>
2014-02-24 17:54:05 -0600

    added keys

---
+++ keys.js
@@ -0,0 +1,24 @@
+GLOBAL.cloudinaryConfig = {
+	cloud_name: 'hfjjoialf',
+	api_key: '464752786413982', 
+  	api_secret: '55mwg-8lfpQQZqx9q6zxj66umDY'
+}
+
+/* 
+NEW KEYS 
+========================== */
+GLOBAL.twitter = {
+	consumerKey: '9fT6SqykLZXhOgrJSIgIw',
+	consumerSecret: 'ARGeh9TtDYsb4skQQ1f12ZCcq29uAEl38tsae9dmkQ',
+	callbackURL: '/auth/twitter/callback'
+}
+
+GLOBAL.facebook = {
+	clientID: '463397427101745',
+	clientSecret: 'ea80b891937de48d79e586d10ee4fb5d',
+	callbackURL: '/auth/facebook/callback'
+}
+
+GLOBAL.mongolab = {
+	URI: 'mongodb://heroku_app17523224:8jb1ejmd4rgnljv0if716mqpla@ds041208.mongolab.com:41208/heroku_app17523224'
+}
